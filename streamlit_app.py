acb638a28b6d14d210d7193cd2a9cbfd3581eca8
Muhammad Umair Mohsin <77578635+umairmohsin@users.noreply.github.com>
2025-06-03 15:47:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ name_on_order = st.text_input("Name on Smoothie :")
 st.write("The name on your Smoothie will be :", name_on_order)
 
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:',
@@ -30,7 +31,7 @@ if ingredients_list:
     ingredients_string= ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+ ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json() , use_container_width = True)
 
